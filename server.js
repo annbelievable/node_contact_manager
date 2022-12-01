8d94ded6505985c37fc3a5883d4c52f9f3f2b35a
kokann <kokann_92@hotmail.com>
2022-12-01 10:49:19 +0800

    Update dotenv configuration in the server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const dotenv = require('dotenv').config();
+
 const app = express();
+
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res, next)=>{
 	res.send('Hello world')
 })
 
-app.listen(3000, ()=>{
-	console.log("App running on port 3000")
+app.listen(port, ()=>{
+	console.log(`Server running on port ${port}`);
 })
