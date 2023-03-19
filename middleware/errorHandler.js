61e808dae060c82112a4eec64c68ae795c7faa52
kokann <kokann_92@hotmail.com>
2023-03-19 22:19:28 +0800

    Fix errorHandler middleware function argument

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const {constants} = require("../constants");
 
-const errorHandler = (err,req, rex, next) => {
+const errorHandler = (req, res, next) => {
 	const statusCode = res.statusCode ? res.statusCode : 500;
 	switch (statusCode) {
 		case constants.VALIDATION_ERROR:
