b12bf95cce42a9e4480cff9aca55f7774f08d033
kokann <kokann_92@hotmail.com>
2023-03-19 23:26:12 +0800

    Fix contact model

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const Contact = new mongoose.Schema({
-	name: { type: String, index: true, required: true },
-	email: { type: String, index: true, required: true },
-	phone: { type: String },
-	User: { type: mongoose.Schema.ObjectId, required: true },
-	timestamp: { type: Date, default: Date.now }
+const schema = new mongoose.Schema({
+  name: { type: String, index: true, required: true },
+  email: { type: String, index: true, required: true },
+  phone: { type: String },
+  User: { type: mongoose.Schema.ObjectId, required: true },
+  timestamp: { type: Date, default: Date.now },
 });
+
+const Contact = mongoose.model("Contact", schema);
 
 module.exports = Contact;
