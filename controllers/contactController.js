e6afcb9858c2551262b8bb79f30fa39779f525ba
kokann <kokann_92@hotmail.com>
2023-03-19 23:28:23 +0800

    Laid out basic contact controllers code

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,62 @@
 const Contact = require("../models/contactModel");
+const asyncHandler = require("express-async-handler");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access private
-const getContacts = asyncHandler(async(req, res) => {
-	const {contacts} = await Contact.find({user_id:req.user.id});
-	res.status(200).json(contacts);
+const getContacts = asyncHandler(async (req, res) => {
+  const { contacts } = await Contact.find({ user_id: req.user.id });
+  res.status(200).json(contacts);
 });
 
-module.exports = {getContacts};
+//@desc Get contact
+//@route GET /api/contacts/:id
+//@access private
+const getContact = asyncHandler(async (req, res) => {
+  const { contact } = await Contact.find({ user_id: req.user.id, id: req.id });
+  res.status(200).json(contact);
+});
+
+//@desc Create contact
+//@route POST /api/contacts
+//@access private
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+
+  if (!name || !email) {
+    res.status(400);
+    throw new Error("Name and email are mandatory!");
+  }
+
+  const contact = await Contact.create({
+    User: req.user,
+    name,
+    email,
+    phone,
+  });
+  res.status(200).json({ contact: contact });
+});
+
+//@desc Update contact
+//@route PUT /api/contacts/:id
+//@access private
+const updateContact = asyncHandler(async (req, res) => {
+  const { contacts } = await Contact.find({ user_id: req.user.id });
+  res.status(200).json(contacts);
+});
+
+//@desc Delete contact
+//@route DELETE /api/contacts/:id
+//@access private
+const deleteContact = asyncHandler(async (req, res) => {
+  const { contacts } = await Contact.find({ user_id: req.user.id });
+  res.status(200).json(contacts);
+});
+
+module.exports = {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+};
