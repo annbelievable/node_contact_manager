5450e77eefc71bf1901e16ea8d3cba1ac9fac74a
kokann <kokann_92@hotmail.com>
2023-05-31 22:07:17 +0800

    Update db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.CONNECTION_STRING);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-	console.log("Connected successfully");
+  console.log("Connected successfully");
 });
 
 module.exports = db;
