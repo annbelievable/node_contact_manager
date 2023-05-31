671e3ffadbb22e7ab3919eec59628530e45e1537
kokann <kokann_92@hotmail.com>
2023-05-31 23:02:26 +0800

    Contact controller fully working now

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -6,18 +6,20 @@ const mongoose = require("mongoose");
 //@route GET /api/contacts
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-  const { contacts } = await Contact.find({
-    User: new mongoose.Types.ObjectId(req.user.id),
-  });
-  res.status(200).json(contacts);
+  const contacts = await Contact.find({ User: req.user.id });
+  res.status(200).json({ contacts: contacts });
 });
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access private
 const getContact = asyncHandler(async (req, res) => {
-  const { contact } = await Contact.find({ user_id: req.user.id, id: req.id });
-  res.status(200).json(contact);
+  const reqId = req.params["id"];
+  const contact = await Contact.find({
+    User: req.user.id,
+    _id: reqId,
+  });
+  res.status(200).json({ contact: contact });
 });
 
 //@desc Create contact
@@ -44,16 +46,36 @@ const createContact = asyncHandler(async (req, res) => {
 //@route PUT /api/contacts/:id
 //@access private
 const updateContact = asyncHandler(async (req, res) => {
-  const { contacts } = await Contact.find({ user_id: req.user.id });
-  res.status(200).json(contacts);
+  const reqId = req.params["id"];
+  let data = { _id: reqId };
+  const { name, email, phone } = req.body;
+  if (name) {
+    data.name = name;
+  }
+  if (email) {
+    data.email = email;
+  }
+  if (phone) {
+    data.phone = phone;
+  }
+  const contact = await Contact.updateOne(data);
+  res.status(200).json(contact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access private
 const deleteContact = asyncHandler(async (req, res) => {
-  const { contacts } = await Contact.find({ user_id: req.user.id });
-  res.status(200).json(contacts);
+  const reqId = req.params["id"];
+  Contact.deleteOne({ _id: reqId }, function (err, results) {
+    if (err) {
+      res.status(400).json({ success: false });
+      return;
+    }
+    console.log("Deleted:", results);
+  });
+
+  res.status(200).json({ success: true });
 });
 
 module.exports = {
