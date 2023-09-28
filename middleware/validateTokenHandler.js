ff89b3381f45e8fe5570eafceee085a9b3484dab
kokann <kokann_92@hotmail.com>
2023-09-28 17:29:27 +0800

    Use tab

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -1,18 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
-  let authHeader = req.headers["authorization"];
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        res.status(401);
-        throw new Error("User is not authorized");
-      }
-      req.user = decoded.user;
-      next();
-    });
-  }
+    let authHeader = req.headers["authorization"];
+    if (authHeader && authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401);
+                throw new Error("User is not authorized");
+            }
+            req.user = decoded.user;
+            next();
+        });
+    }
 };
 
 module.exports = { validateToken };
